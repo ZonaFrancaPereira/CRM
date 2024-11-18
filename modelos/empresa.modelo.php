92fa018e120a4501187c4c4d220946a776429e6a
Melissa <ymontoyag@zonafrancadepereira.com>
2024-11-18 11:48:00 -0500

    categ

--- modelos/empresa.modelo.php
+++ modelos/empresa.modelo.php
@@ -86,7 +86,7 @@ public static function mdlMostraEmpresas($tabla, $item, $valor)
 }
 
 
-	public static function SmdlMostraEmpresasAsignada($tabla, $consulta)
+	public static function mdlMostrarEmpresas($tabla, $consulta)
 	{
 		switch ($consulta) {
 			
