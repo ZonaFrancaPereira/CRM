d1130a2376eba6ca5f5788eb713976089cf513ac
Melissa <ymontoyag@zonafrancadepereira.com>
2025-02-14 13:54:10 -0500

    melissa

--- vistas/modulos/perfilUsuario.php
+++ vistas/modulos/perfilUsuario.php
@@ -1,5 +1,5 @@
-	 <!-- Sidebar Menu -->
-     <nav class="mt-2">
+	  <!-- Sidebar Menu -->
+ <nav class="mt-2">
    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
      <li class="nav-item En_linea 1" role="presentation">
@@ -52,8 +52,35 @@
  </div>
  <!-- /.sidebar -->
  </aside>
-    <!-- Main content -->
-	<section class="content">
+
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+   <!-- Content Header (Page header) -->
+   <div class="content-header">
+     <div class="container-fluid">
+       <div class="row mb-2">
+         <div class="col-sm-6">
+           <h1 class="m-0">Perfil</h1>
+         </div><!-- /.col -->
+         <div class="col-sm-6">
+           <ol class="breadcrumb float-sm-right">
+             <li class="breadcrumb-item"><a href="#">Home</a></li>
+             <li class="breadcrumb-item active">Perfil v1</li>
+           </ol>
+         </div><!-- /.col -->
+       </div><!-- /.row -->
+     </div><!-- /.container-fluid -->
+   </div>
+   <!-- /.content-header -->
+
+   <!-- Main content -->
+   <div class="container-fluid">
+     <div class="row">
+
+
+       <!-- Calendario -->
+       <div class="col-md-12">
+	   <section class="content">
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-3">
@@ -181,8 +208,14 @@
 			<!-- /.row -->
 		</div><!-- /.container-fluid -->
 	</section>
-	<!-- /.content -->
-</div>
+
+       </div>
+     </div>
+   </div>
+
+ </div>
+	 
+
 
 
 
